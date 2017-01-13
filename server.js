572797b68d031132aae78686f0ee4885abaa9b5b
shehantissera <shehan@duosoftware.com>
2017-01-13 10:22:36 +0530

    added the public IP

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ var options = {
   server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } },
   replset: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } }
 };
-mongoose.connect('mongodb://shehan:doktertislab@127.0.0.1/rest_test',options);
+mongoose.connect('mongodb://shehan:doktertislab@128.199.153.75/rest_test',options);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
